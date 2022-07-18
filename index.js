d29796228fc57822fefc2d95c7f56562dd95b53a
ehab0barakat <mrehab9797@gmail.com>
2022-07-19 00:26:33 +0200

    register && login

--- index.js
+++ index.js
@@ -24,20 +24,50 @@ connection.connect(function(error){
 });
 
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    // connection.query('select * from users', (err, rows) => {
-    //     if (!err)
-    //         res.send(rows);
-    //     else
-    //         console.log(err);
-    // })
-    res.render("index");
-});
-
-
 app.listen(4000);
 
 
+// -------------------- ( login && register started ) -----------------------
+const path = require('path');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
+dotenv.config({ path: './.env'});
+
+const db = mysql.createConnection({
+	host: process.env.DATABASE_HOST,
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	database: process.env.DATABASE
+});
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: false }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(cookieParser());
+
+app.set('view engine', 'hbs');
+
+db.connect( (error) => {
+  if(error) {
+    console.log(error)
+  } else {
+    console.log("MYSQL Connected...")
+  }
+})
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(5001, () => {
+  console.log("Server started on Port 5001");
+})
+
+// -------------------- ( login && register ended ) -----------------------
 
 
