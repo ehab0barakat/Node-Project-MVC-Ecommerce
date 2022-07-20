d1e0c79d951523ac0b0371864ee9d42a1caa7cdb
ehab0barakat <mrehab9797@gmail.com>
2022-07-20 02:09:32 +0200

    dd

--- routes/pages.js
+++ routes/pages.js
@@ -5,11 +5,11 @@ var mysql = require('mysql');
 const cors = require('cors');
 
 var db = mysql.createConnection({
-	host:'localhost',
-	user:'root',
-  port:'3306',
-	password:'',
-	database:'node_project'
+  host: 'localhost',
+  user: 'root',
+  port: '3306',
+  password: '',
+  database: 'node_project'
 });
 
 
@@ -28,7 +28,7 @@ router.get('/', authController.isLoggedIn, (req, res) => {
 router.get('/register', authController.isLoggedIn, (req, res) => {
   db.query('SELECT * FROM users WHERE email =?', [req.cookies.email], (error, result) => {
     req.user = result;
-    res.render('register', { message : "" , user: req.user});
+    res.render('register', { message: "", user: req.user });
   });
 });
 
@@ -37,10 +37,10 @@ router.get('/register', authController.isLoggedIn, (req, res) => {
 
 router.get('/login', authController.isLoggedIn, (req, res) => {
   // db.query('SELECT * FROM users WHERE email =?', [req.cookies.email], (error, result) => {
-    // console.log(result);
-    // req.user = result;
-    res.render('login' , { message : "" , user: req.user});
-    // res.render('register', { message : "" , user: req.user});
+  // console.log(result);
+  // req.user = result;
+  res.render('login', { message: "", user: req.user });
+  // res.render('register', { message : "" , user: req.user});
   // });
 });
 
@@ -48,41 +48,179 @@ router.get('/login', authController.isLoggedIn, (req, res) => {
 // -------------------------------------
 
 router.get('/profile', authController.isLoggedIn, (req, res) => {
-  
-// console.log( req.user[0] );
 
-  if( req.user[0] ) {
+  // console.log( req.user[0] );
+
+  if (req.user[0]) {
 
     // console.log(req.user[0]);
-    
-    res.render('profile', { user : req.user[0] });
+
+    res.render('profile', { user: req.user[0] });
   } else {
     res.redirect('/login');
   }
+
+});
+
+//----------------------(description)---------------
+router.get('/description/:id',  authController.isLoggedIn , function(req, res) {
+    
+
+
   
+  var id =req.params.id;
+  var sql = 'SELECT *  FROM products WHERE ID = ?';
+
+  db.query(sql,[id], function(err, result, fields) {
+      if (err){
+          throw err;
+      } else {
+        res.render('description', {title: 'description', products: result , user : req.user });
+
+      }
+  });
 });
 
 // ------------------------------  ( shop ) ----------------------------------
 
-router.get('/shop',  authController.isLoggedIn , function(req, res) {
-  console.log("9999999")
-  console.log(req.user);
-  console.log("9999999")
+router.get('/shop', authController.isLoggedIn, function (req, res) {
+
   db.query("SELECT * FROM products ", function (err, result, fields) {
     if (err) {
       throw err;
-	  } else {
-      res.render('products', {title: 'Shop', products: result , user : req.user });
-	  }
-	});
+    } else {
+      res.render('products', { title: 'Shop', products: result, user: req.user });
+    }
+  });
+});
+
+
+
+// ------------------------------  ( seller ) ----------------------------------
+
+router.get('/seller', authController.isLoggedIn, function (req, res) {
+
+    console.log("wwwwwwwwww")
+    console.log(req.user)
+    console.log("wwwwwwwwww")
+  db.query("SELECT * FROM products where seller_id =? " ,[req.user[0].ID] , function (err, result, fields) {
+    if (err) {
+      throw err;
+    } else {
+      res.render('sellerPage', { title: 'sellerPage', products: result, user: req.user });
+    }
+  });
+});
+
+
+// ADD product 
+router.get('/addproducts',authController.isLoggedIn, function (req, res, next) {
+  res.render('addproducts', {
+    title: 'Add New Products',
+    ID: '',
+    name: '',
+    seller_id: '',
+    price: '',
+    image: '',
+    description: '',
+    user: req.user
+  })
+})
+
+
+//
+router.post('addproducts',authController.isLoggedIn, function (req, res, next) {
+
+console.log("ehba")
+  
+db.query('INSERT INTO products SET ?', product, function (err, result) {
+    if (err) {
+      req.flash('error', err)
+      res.render('seller/addproducts', {
+        title: 'Add New product',
+        ID: product.id,
+        name: product.name,
+        seller_id: seller.id,
+        price: product.price,
+        image: product.image,
+        description: product.description,
+        user: req.user
+
+      })
+      res.redirect('/seller');
+    } else {
+      // req.flash('success', 'Data added successfully!');
+      res.redirect('/seller');
+    }
+  })
+})
+
+// EDIT product FORM
+router.get('/editproduct/:id',authController.isLoggedIn, function (req, res, next) {
+  
+  db.query('SELECT * FROM products WHERE id = ' + req.params.id, function (err, rows, fields) {
+    if (err) {
+      throw err
+    }
+    else {
+
+      res.render('editproduct', {
+        title: 'Edit product',
+        ID: rows[0].ID,
+        name: rows[0].name,
+        seller_id: rows[0].seller_id,
+        price: rows[0].price,
+        image: rows[0].image,
+        description: rows[0].description, user: req.user
+      })
+    }
+  })
+});
+
+// EDIT product
+router.post('/updateproduct/:id',authController.isLoggedIn, function (req, res, next) {
+  db.query('UPDATE products SET ? WHERE id = ' + req.params.id, product, function (err, result) {
+    if (err) {
+      res.render('editproduct', {
+        title: 'Edit product',
+        ID: product.id,
+        name: product.name,
+        seller_id: seller.id,
+        price: product.price,
+        image: product.image,
+        description: product.description, user: req.user
+      })
+    } else {
+      res.redirect('sellerPage');
+    }
+  })
+});
+
+
+// DELETE product
+router.get('/deleteproduct/(:id)',authController.isLoggedIn, function (req, res, next) {
+
+  console.log( req.params.id );
+  
+  db.query(' delete FROM products WHERE ID = ?' , [+req.params.id], function (err, result) {
+    
+      if (err) {
+        throw err;
+      } else {
+    console.log( result );
+    // res.redirect('/seller')
+  }
+  })
 });
 
 // ------------------------------  ( add to cart ) ----------------------------------
 
 router.post('/search', authController.isLoggedIn,function(req,res){
   var str = {
-      stringPart:req.body.spearhead
+    stringPart: req.body.spearhead
   }
+
+
   db.query('SELECT * FROM products WHERE products.name LIKE "%'+str.stringPart+'%"',function(err, result, fields) {
     console.log(result);
     if (err) {
@@ -93,29 +231,27 @@ router.post('/search', authController.isLoggedIn,function(req,res){
   });
 });
 
-router.get('/add_to_cart/:id', authController.isLoggedIn, function (req, res){
-    
+  router.get('/add_to_cart/:id', authController.isLoggedIn, function (req, res) {
+
     // console.log(res);
-   var id =req.params.id;
-   var user_id=req.user[0].ID
+    var id = req.params.id;
+    var user_id = req.user[0].ID
 
     // var sql = 'SELECT * FROM products WHERE ID = id';
-    db.query('INSERT INTO cart SET ?', { user_id: user_id , product_id: id}, (error, results) => {
-       console.log(error);
-    });
-   
-        res.redirect('/shop');
+    db.query('INSERT INTO cart SET ?', { user_id: user_id, product_id: id }, (error, results) => {
+      console.log(error);
 
-  
-});
+    });
+    res.redirect('/shop');
+  });
 
 
 
 
 // ------------------------------  ( remove from cart ) ----------------------------------
 
-router.get('/remove_product/:id', authController.isLoggedIn,function(req,res){
-     
+router.get('/remove_product/:id', authController.isLoggedIn, function (req, res) {
+
   //  var proId=req.params.id;
   
         var id =req.params.id;
@@ -136,31 +272,31 @@ router.get('/remove_product/:id', authController.isLoggedIn,function(req,res){
 
 // ------------------------------  ( cart products ) ----------------------------------
 
-router.get('/cart', authController.isLoggedIn,function(req,res){
-  var id=req.user[0].ID;
-  
-    // console.log(req.user[0].ID);
+router.get('/cart', authController.isLoggedIn, function (req, res) {
+  var id = req.user[0].ID;
+
+  // console.log(req.user[0].ID);
   var sql = "SELECT products.ID, products.name , products.image, products.price  FROM products RIGHT JOIN cart ON products.ID = Product_id   WHERE cart.user_id =?";
-  
-  db.query(sql,[id], function(err, results, fields) {
-        if (err){
-            throw err;
-        } else {
-          //  console.log(results);
-          let totalPrice = 0 ;
-          for (const prod of results) {
-            totalPrice+= prod.price
-          }
-            
-        res.render('cart', {title: 'cart', user : req.user,products: results,total:totalPrice});
-        
-        }
-      });
+
+  db.query(sql, [id], function (err, results, fields) {
+    if (err) {
+      throw err;
+    } else {
+      //  console.log(results);
+      let totalPrice = 0;
+      for (const prod of results) {
+        totalPrice += prod.price
+      }
+
+      res.render('cart', { title: 'cart', user: req.user, products: results, total: totalPrice });
+
+    }
+  });
 
 });
 // ------------------------------  ( check out ) ----------------------------------
 
-router.get('/checkout',authController.isLoggedIn,function(req,res){
+router.get('/checkout', authController.isLoggedIn, function (req, res) {
 
   var id=req.user[0].ID;
   var sql = "SELECT  cart.product_id  FROM cart WHERE cart.user_id =?";
@@ -205,13 +341,13 @@ router.get('/order',  authController.isLoggedIn , function(req, res) {
       if (result.length != 0){
         db.query(`SELECT * from products where id in ${result[0].product_ids}`, function (err, result, fields) {
           if (err) {
-          throw err;
+            throw err;
           } else {
-            var tot = 0 ;
+            var tot = 0;
             for (const prod of result) {
               tot += prod.price
             }
-            res.render('order', { products: result , user : req.user , total : tot});
+            res.render('order', { products: result, user: req.user, total: tot });
           };
         });
       }else{
@@ -228,17 +364,17 @@ router.get('/order',  authController.isLoggedIn , function(req, res) {
 
 // ------------------------------  ( thanks page ) ----------------------------------
 
-router.get('/thankyou',  authController.isLoggedIn , function(req, res) {
-  
-  db.query(`select ID from users WHERE email =?`, [req.cookies.email] , function (err, result, fields) {
+router.get('/thankyou', authController.isLoggedIn, function (req, res) {
+
+  db.query(`select ID from users WHERE email =?`, [req.cookies.email], function (err, result, fields) {
     if (err) {
-    throw err;
+      throw err;
     } else {
-      db.query(`DELETE FROM cart WHERE user_id =?`, [result[0].ID] , function (err, result, fields) {
+      db.query(`DELETE FROM cart WHERE user_id =?`, [result[0].ID], function (err, result, fields) {
         if (err) {
           throw err;
         } else {
-          res.render('thankyou', { user : req.user });
+          res.render('thankyou', { user: req.user });
         }
       });
     }
